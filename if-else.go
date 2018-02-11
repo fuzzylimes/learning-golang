52c1ad92ef6947a3d2ab9e87321383cae0ee2a80
zcochran <fuzzylimes@gmail.com>
2018-02-10 21:38:10 -0500

    if-else statements in go

---
+++ if-else.go
@@ -0,0 +1,27 @@
+// if/else statements in Go. Pretty much the same as in every other language. Nothing special.
+
+package main
+
+import "fmt"
+
+func main() {
+
+	if 7%2 == 0 {
+		fmt.Println("7 is even")
+	} else {
+		fmt.Println("7 is odd")
+	}
+
+	if 8%4 == 0 {
+		fmt.Println("8 is even")
+	}
+
+	// Nothing special here, just a neat example of creating a new variable in the same line as making the evaluation
+	if num := 9; num < 0 {
+		fmt.Println(num, "is negative")
+	} else if num < 10 {
+		fmt.Println(num, "has 1 digit")
+	} else {
+		fmt.Println(num, "has multiple digits")
+	}
+}
